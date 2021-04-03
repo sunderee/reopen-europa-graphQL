e74921173176e134f000d31952bb3fc05ab1c462
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-04-03 19:35:06 +0200

    No need for a pipe + bug fix related to imports in the info module

--- src/info/info.module.ts
+++ src/info/info.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InfoService } from './info.service';
 import { InfoResolver } from './info.resolver';
+import { ApiModule } from '../api/api.module';
 
 @Module({
+    imports: [ApiModule],
     providers: [InfoService, InfoResolver]
 })
 export class InfoModule {}
